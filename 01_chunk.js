ed3892c608a63155927b37fe19d230b280cad9f2
olhkyle <kylekwon.dev@gmail.com>
2025-03-15 00:06:09 +0900

    first-commit

---
+++ 01_chunk.js
@@ -0,0 +1,13 @@
+/**
+ * Problem 1 : Chunk
+ *
+ * Implement as function that splits an array into groups of size N.
+ * The last chunk may contain less elements than N.
+ *
+ * @example
+ * chunk([1,2,3,4,5],2) => [[1,2], [3,4], [5]]
+ * chunk([1,2,3],1) => [[1], [2], [3]]
+ * chunk([], 5) => []
+ */
+
+export const chunk = (arr, size = 1) => {};
---
+++ 06_debounceAndThrottle.js
@@ -0,0 +1,2 @@
+const debounce = () => {};
+const throttle = () => {};
