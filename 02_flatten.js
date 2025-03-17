da46633dcb0a1de9863f5e9261606a0be37c3ac0
olhkyle <kylekwon.dev@gmail.com>
2025-03-17 14:09:34 +0900

    feat : develop flatten.js

--- 02_flatten.js
+++ 02_flatten.js
@@ -1,1 +1,27 @@
-const flatten = () => {};
+/**
+ * Problem 2 : Flatten
+ *
+ * Implement a function that flattens a nested array structure
+ * The function should accept an optional depth parameter.
+ *
+ * @example
+ * flatten([1, [2, 3], [4, [5, 6]]])     // => [1,2,3,4,5,6]
+ * flatten([1, [2, [3, [4]] ] ], 2)      // => [1, 2, 3, [4]]
+ * flatten([1, [], [2, []], []])       // => [1,2]
+ * flatten([1, [2, [3, [4, [5]]]]])    // => [1,2,3,4,5]
+ */
+
+const flatten = (array, depth = Infinity) => {
+	return depth > 0
+		? array.reduce((acc, item) => {
+				const currentItem = Array.isArray(item) ? flatten(item, depth - 1) : item;
+
+				return acc.concat(currentItem); // returns a new merged array
+			}, [])
+		: [...array];
+};
+
+console.log(flatten([1, [2, 3], [4, [5, 6]]]));
+console.log(flatten([1, [2, [3, [4]]]], 2));
+console.log(flatten([1, [], [2, []], []]));
+console.log(flatten([1, [2, [3, [4, [5]]]]]));
